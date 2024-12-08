a66b82b1e318fe9cf86089ccabd6de8839e68f7a
StevenBuglione <stevenbuglione1@gmail.com>
2024-12-08 00:03:56 -0500

    update

--- modules/system/wm/gnome.nix
+++ modules/system/wm/gnome.nix
@@ -1,9 +1,20 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  userSettings,
+  ...
+}:
 
 {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.autoLogin.enable = true; # Enable automatic login for the user.
+  services.displayManager.autoLogin.user = "${userSettings.username}";
+
+  systemd.services."getty@tty1".enable = false; # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."autovt@tty1".enable = false;
+
   services.xserver.xkb = {
     layout = "us";
     variant = "";
